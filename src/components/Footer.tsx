91450d4f4a3b60f29772dcc36c38c3574cbad4de
Fabio <lazzaroni@jeflab.it>
2021-02-10 16:08:23 +0100

    💄 Added gatsby icon to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
 import React, {VFC} from "react";
 import {version} from "../../package.json";
-import {footer, inner} from "./Footer.module.scss";
+import {footer, inner, gatsbyLogo} from "./Footer.module.scss";
+import gatsby from "../images/gatsby-logo.svg";
 
 export const Footer: VFC = () => {
   return (
     <footer className={footer}>
       <div className={inner}>
-        <span>2014 - {new Date().getFullYear()} | Built with ❤ and ☕</span>
+        <span>
+          2014 - {new Date().getFullYear()} | Built with ❤,{" "}
+          <img src={gatsby} alt="gatsby" height="17" className={gatsbyLogo} />{" "}
+          and ☕
+        </span>
         <span>
           <small>v{version}</small>
         </span>
