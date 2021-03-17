f5dca9b63e21dfdea3a995da94ce676336294b32
Fabio <lazzaroni@jeflab.it>
2021-03-17 23:18:59 +0100

    ⬆ Upgrading dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-scss-typescript",
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
       options: {
