9f621b905cf67cba350e637a6f63af2ac4255f35
Fabio <lazzaroni@jeflab.it>
2021-03-23 17:53:02 +0100

    ⬆ Moving to the new gatsby-image

--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,32 +1,21 @@
-import {graphql, useStaticQuery} from "gatsby";
-import GatsbyImage from "gatsby-image";
+import {StaticImage} from "gatsby-plugin-image";
 import React, {VFC} from "react";
 
 export const ProfilePicture: VFC = () => {
-  const {
-    file: {
-      childImageSharp: {fixed},
-    },
-  } = useStaticQuery(graphql`
-    query {
-      file(relativePath: {eq: "fabio.jpg"}) {
-        childImageSharp {
-          fixed(
-            width: 200
-            height: 200
-            traceSVG: {
-              background: "#050505"
-              color: "#757575"
-              threshold: 87
-              blackOnWhite: false
-            }
-          ) {
-            ...GatsbyImageSharpFixed_tracedSVG
-          }
-        }
-      }
-    }
-  `);
-
-  return <GatsbyImage fixed={fixed} alt="Fabio Lazzaroni profile picture" />;
+  return (
+    <StaticImage
+      src="../images/fabio.jpg"
+      alt="Fabio Lazzaroni profile picture"
+      layout="fixed"
+      height={200}
+      width={200}
+      placeholder="tracedSVG"
+      tracedSVGOptions={{
+        background: "#050505",
+        color: "#757575",
+        threshold: 87,
+        blackOnWhite: false,
+      }}
+    />
+  );
 };
